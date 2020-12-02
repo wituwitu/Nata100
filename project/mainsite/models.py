cfaadac5de7609339bfafe327ad81960b08e91b2
wituwitu <diegoswlt@gmail.com>
2020-12-01 21:57:02 -0300

    Agregado modelo de marcas, de las cuales se componen los rankings

--- project/mainsite/models.py
+++ project/mainsite/models.py
@@ -16,7 +16,7 @@ class User(AbstractUser):
     nacimiento = models.DateField(null=True)
     region = models.CharField(max_length=32, default="")
     comuna = models.CharField(max_length=32, default="")
-    amigues = models.ManyToManyField("self", blank=True)    # many (user) to many (user)
+    amigues = models.ManyToManyField("self", blank=True)  # many (user) to many (user)
     # Setting up some variables
     USERNAME_FIELD = 'username'
     EMAIL_FIELD = 'email'
@@ -39,31 +39,44 @@ class Alumne(User):
         return self.username
 
 
+class Marca(models.Model):
+    categoria = models.CharField(max_length=32, unique=True)
+    tiempo = models.TimeField(blank=True, null=True)
+    usuarie = models.ForeignKey(Alumne, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        ordering = ['tiempo']
+
+
 class AbstractRanking(models.Model):
-    categoria = models.CharField(max_length=32, unique=True, primary_key=True)
-    tiempo = models.DateTimeField()
+    marcas = models.ManyToManyField(Marca)  # many (marcas) to many (rankings)
+
 
 class RankingRegional(AbstractRanking):
-    tipo='regional'
+    tipo = 'regional'
     region = models.CharField(max_length=32, unique=True, primary_key=True)
 
     def __str__(self):
         return self.tipo
 
+
 class RankingComunal(AbstractRanking):
-     tipo='comunal'
-     comuna = models.CharField(max_length=32, unique=True, primary_key=True)
-     def __str__(self):
-         return self.tipo
+    tipo = 'comunal'
+    comuna = models.CharField(max_length=32, unique=True, primary_key=True)
+
+    def __str__(self):
+        return self.tipo
+
 
 class RankingNacional(AbstractRanking):
-     tipo='nacional'
+    tipo = 'nacional'
 
-     def __str__(self):
-         return self.tipo
+    def __str__(self):
+        return self.tipo
 
-#- Crear los modelos para usuarios (listo), marcas, ranking, amigos y comentarios
-#- Crear cronómetro (o ingreso de datos manual), que guarde el tiempo asociado al alumne, estilo de nado y otros datos importantes
-#- Escoger si los datos se subirán a algún ranking o no
-#- Recoger datos hacia los ránkings (tabla de mejores posiciones)
+
+# - Crear los modelos para usuarios (listo), marcas, ranking, amigos y comentarios
+# - Crear cronómetro (o ingreso de datos manual), que guarde el tiempo asociado al alumne, estilo de nado y otros datos importantes
+# - Escoger si los datos se subirán a algún ranking o no
+# - Recoger datos hacia los ránkings (tabla de mejores posiciones)
 print('PyCharm')
